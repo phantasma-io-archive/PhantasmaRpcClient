9598122eff52d2c8a7178b274a17c84da4d4b551
Bruno Freitas <bruno_freitas_2015@outlook.com>
2019-10-07 18:48:57 +0100

    getPlatforms update

--- Phantasma.RpcClient/DTOs/PlatformDto.cs
+++ Phantasma.RpcClient/DTOs/PlatformDto.cs
@@ -8,13 +8,25 @@ namespace Phantasma.RpcClient.DTOs
         [JsonProperty("platform")]
         public string Platform { get; set; }
 
-        [JsonProperty("address")]
-        public string Address { get; set; }
+        [JsonProperty("chain")]
+        public string ChainAddress { get; set; }
+
+        [JsonProperty("fuel")]
+        public string FuelSymbol { get; set; }
 
         [JsonProperty("interop")]
         public string Interop { get; set; }
 
         [JsonProperty("tokens")]
         public List<string> Tokens { get; set; }
+    }
+
+    public class Interop
+    {
+        [JsonProperty("local")]
+        public string Local { get; set; }
+
+        [JsonProperty("external")]
+        public string External { get; set; }
     }
 }
